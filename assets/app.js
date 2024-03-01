3b356ec01077990d174689267f80bd6b947c7aa9
Alexis CAPLIEZ <capliezalexis@yahoo.fr>
2024-03-01 18:17:41 +0100

    Add: import all scss

---
+++ assets/app.js
@@ -0,0 +1,19 @@
+import './bootstrap.js';
+/*
+ * Welcome to your app's main JavaScript file!
+ *
+ * We recommend including the built version of this JavaScript file
+ * (and its CSS file) in your base layout (base.html.twig).
+ */
+
+// any CSS you import will output into a single css file (main.scss in this case)
+import 'bootstrap/scss/bootstrap.scss';
+import './styles/main.scss';
+import './styles/_size.scss';
+import './styles/_line-height.scss';
+import './styles/_font-weight.scss';
+import './styles/responsive.scss';
+import './styles/breadcrumb/main.scss';
+import './styles/header/main.scss';
+import './styles/discover/main.scss';
+import './styles/footer/main.scss';
