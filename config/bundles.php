8ef038e8ed41de6e9244e2d690f3b79b2e741748
Alexis CAPLIEZ <capliezalexis@yahoo.fr>
2024-03-01 18:16:09 +0100

    Add: conf

--- config/bundles.php
+++ config/bundles.php
@@ -4,4 +4,6 @@ return [
     Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
     Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
+    Symfony\WebpackEncoreBundle\WebpackEncoreBundle::class => ['all' => true],
+    Symfony\UX\StimulusBundle\StimulusBundle::class => ['all' => true],
 ];
